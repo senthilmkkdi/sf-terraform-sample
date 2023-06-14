1f44fa6e7478d6b4de7cacc2171a639296994f34
senthilmkkdi <83176120+senthilmkkdi@users.noreply.github.com>
2023-06-14 17:04:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source = "Snowflake-Labs/snowflake"
+      version = "0.66.2"
+    }
+  }
+}
 
+provider "snowflake" { 
+  # Configuration options 
+}
